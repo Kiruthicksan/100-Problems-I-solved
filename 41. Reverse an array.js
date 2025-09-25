d24038ff8f684cae57a9e40cedffbd747d9ae3af
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-09-25 10:31:29 +0530

    reverse an array

---
+++ 41. Reverse an array.js
@@ -0,0 +1,14 @@
+function reverseAnArray(arr){
+    let left = 0
+    let right = arr.length - 1
+
+    while (left < right){
+        [arr[left], arr[right]] = [arr[right], arr[left]]
+        left++
+        right--
+    }
+
+    return arr
+}
+
+console.log(reverseAnArray([1,2,3,4,5]))
