1635dc1e037b8b1f841d586e38f52959a22dbee8
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-10-12 10:35:31 +0530

    Best time to sell stock

--- 56. RemoveDuplicates.js
+++ 56. RemoveDuplicates.js
@@ -1,14 +1,14 @@
 function removeDuplicates(nums) {
-  let k = 0;
+  let k = 0
 
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[k] !== nums[i]) {
-      k++;
-      nums[k] = nums[i];
+  for (let i = 1; i < nums.length; i++){
+    if(nums[i] !== nums[k]){
+      k++
+      nums[k] = nums[i]
     }
   }
 
-  return k + 1;
+  return k + 1
 }
 
 console.log(removeDuplicates([1, 1, 2]));
